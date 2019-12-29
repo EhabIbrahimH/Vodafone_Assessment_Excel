c609aacd2656eaf2a19ea2d9d180945516e8b8fd
Ehab <e.hassan@nu.edu.eg>
2019-12-29 20:29:38 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,186 @@
+import React, { Component } from "react";
+import "./App.css";
+import { OutTable, ExcelRenderer } from "react-excel-renderer";
+import {
+  Jumbotron,
+  Col,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  FormGroup,
+  Label,
+  Button,
+  Fade,
+  FormFeedback,
+  Container,
+  Card
+} from "reactstrap";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      dataLoaded: false,
+      isFormInvalid: false,
+      rows: null,
+      cols: null
+    };
+    this.fileHandler = this.fileHandler.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.openFileBrowser = this.openFileBrowser.bind(this);
+    this.renderFile = this.renderFile.bind(this);
+    this.openNewPage = this.openNewPage.bind(this);
+    this.fileInput = React.createRef();
+  }
+
+  renderFile = fileObj => {
+    //just pass the fileObj as parameter
+    ExcelRenderer(fileObj, (err, resp) => {
+      if (err) {
+        console.log(err);
+      } else {
+        this.setState({
+          dataLoaded: true,
+          cols: resp.cols,
+          rows: resp.rows
+        });
+      }
+    });
+  };
+
+  fileHandler = event => {
+    if (event.target.files.length) {
+      let fileObj = event.target.files[0];
+      let fileName = fileObj.name;
+
+      //check for file extension and pass only if it is .xlsx and display error message otherwise
+      if (fileName.slice(fileName.lastIndexOf(".") + 1) === "xlsx") {
+        this.setState({
+          uploadedFileName: fileName,
+          isFormInvalid: false
+        });
+        this.renderFile(fileObj);
+      } else {
+        this.setState({
+          isFormInvalid: true,
+          uploadedFileName: ""
+        });
+      }
+    }
+  };
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  openFileBrowser = () => {
+    this.fileInput.current.click();
+  };
+
+  openNewPage = chosenItem => {
+    const url =
+      chosenItem === "github"
+        ? "https://github.com/EhabIbrahimH"
+        : "https://www.linkedin.com/in/ehabibrahem/";
+    window.open(url, "_blank");
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <Jumbotron className="jumbotron-background">
+            <h1 className="display-3" style={{ color: "white", zIndex: 0 }}>
+              Excel Renderer
+            </h1>
+            <p className="lead" style={{ color: "white", zIndex: 0 }}>
+              This is a part of Vodafone's internship Interview process
+            </p>
+            <Button
+              className="primary jumbotron-button"
+              onClick={this.openNewPage.bind(this, "github")}
+            >
+              GitHub
+            </Button>{" "}
+            <Button
+              className="primary jumbotron-button"
+              onClick={this.openNewPage.bind(this, "linkedin")}
+            >
+              LinkedIn
+            </Button>
+            <hr className="my-2" />
+            <p style={{ color: "white", zIndex: 0 }}>
+              Developed By Ehab Ibrahim
+            </p>
+          </Jumbotron>
+        </div>
+        <Container>
+          <form>
+            <FormGroup row>
+              <Label for="exampleFile" xs={6} sm={4} lg={2} size="lg">
+                Upload
+              </Label>
+              <Col xs={4} sm={8} lg={10}>
+                <InputGroup>
+                  <InputGroupAddon addonType="prepend">
+                    <Button
+                      color="info"
+                      style={{ color: "white", zIndex: 0 }}
+                      onClick={this.openFileBrowser.bind(this)}
+                    >
+                      <i className="cui-file"></i> Browse&hellip;
+                    </Button>
+                    <input
+                      type="file"
+                      hidden
+                      onChange={this.fileHandler.bind(this)}
+                      ref={this.fileInput}
+                      onClick={event => {
+                        event.target.value = null;
+                      }}
+                      style={{ padding: "10px" }}
+                    />
+                  </InputGroupAddon>
+                  <Input
+                    type="text"
+                    className="form-control"
+                    value={this.state.uploadedFileName}
+                    readOnly
+                    invalid={this.state.isFormInvalid}
+                  />
+                  <FormFeedback>
+                    <Fade
+                      in={this.state.isFormInvalid}
+                      tag="h6"
+                      style={{ fontStyle: "italic" }}
+                    >
+                      Please select a .xlsx file only !
+                    </Fade>
+                  </FormFeedback>
+                </InputGroup>
+              </Col>
+            </FormGroup>
+          </form>
+
+          {this.state.dataLoaded && (
+            <div>
+              <Card body outline color="secondary" className="restrict-card">
+                <OutTable
+                  data={this.state.rows}
+                  columns={this.state.cols}
+                  tableClassName="ExcelTable2007"
+                  tableHeaderRowClass="heading"
+                />
+              </Card>
+            </div>
+          )}
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
